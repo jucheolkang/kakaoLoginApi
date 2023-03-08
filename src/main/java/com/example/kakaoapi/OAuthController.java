b78d262cd8fab88843fafad4e3df159428545345
jucheolkang <jucheol9877@naver.com>
2023-03-08 16:44:24 +0900

    add OAuthService

--- src/main/java/com/example/kakaoapi/OAuthController.java
+++ src/main/java/com/example/kakaoapi/OAuthController.java
@@ -1,20 +1,27 @@
 package com.example.kakaoapi;
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/outh")
+@RequestMapping("/oauth")
 public class OAuthController {
     /*
     카카오 callback
     * [GET]/outh/kakao/callback
      */
+    @Autowired
+    private OAuthService kakao;
     @ResponseBody
     @GetMapping("/kakao")
-    public Object kakaoCallback(@RequestParam String code) {
-        System.out.println(code);
-        return code;
+    public void kakaoCallback(@RequestParam(value="code", required=false)  String code) {
+
+
+        String access_Token = kakao.getKakaoAccessToken(code);
+        System.out.println("controller access_token : " + access_Token);
     }
+
+
 }
